b4ae8748a20fb1cb74a61f6ca83f7100cff256c7
Alvin Xu <alvin3xu1@gmail.com>
2023-10-24 01:07:51 -0700

    setup js functions

--- js/dom_manip.js
+++ js/dom_manip.js
@@ -3,7 +3,27 @@
     The main() function at the bottom provides an example of using functions from fetch_api.js to retrieve data from the API
 */
 
-// main function to run when this is run in Node.js
+// use this function to setup the home/launch page
+const setupHome = async () => {
+
+};
+
+// use this function to set up an individual question
+const setupQuestion = (question) => {
+
+}
+
+// use this function to start the quiz
+const beginQuiz = async () => {
+
+};
+
+// use this function to show the player's final score
+const setupResults = () => {
+
+};
+
+// main function
 const main = async function() {
     const categories = await getCategories();
     categories.unshift({id: 0, name: "All Categories"});
